faea1c1a7cf665dac19372316f9401320543444d
kimaxq <73841363+kimaxq@users.noreply.github.com>
2020-12-22 14:33:06 +0600

    Update project.py

--- project.py
+++ project.py
@@ -4,83 +4,83 @@ import random
 import telebot
 bot = telebot.TeleBot('1088440063:AAGOhaUT8At4WBVRsgHfYRxZ-5Xll5pV1zA')
 
-first = ["Сегодня — идеальный день для новых начинаний.",
-         "Оптимальный день для того, чтобы решиться на смелый поступок!",
-         "Будьте осторожны, сегодня звёзды"
-         " могут повлиять на ваше финансовое состояние.",
-         "Лучшее время для того, чтобы начать"
-         " новые отношения или разобраться со старыми.",
-         "Плодотворный день для того, чтобы "
-         "разобраться с накопившимися делами."]
-second = ["Но помните, что даже в этом случае нужно не забывать про",
-          "Если поедете за город, заранее подумайте про",
-          "Те, кто сегодня нацелен выполнить "
-          "множество дел, должны помнить про",
-          "Если у вас упадок сил, обратите внимание на",
-          "Помните, что мысли материальны, а значит вам"
-          " в течение дня нужно постоянно думать про"]
-second_add = ["отношения с друзьями и близкими.",
-              "работу и деловые вопросы, "
-              "которые могут так некстати помешать планам.",
-              "себя и своё здоровье, иначе к "
-              "вечеру возможен полный раздрай.",
-              "бытовые вопросы — особенно те,"
-              " которые вы не доделали вчера.",
-              "отдых, чтобы не превратить себя в"
-              " загнанную лошадь в конце месяца."]
-third = ["Злые языки могут говорить вам обратное,"
-         " но сегодня их слушать не нужно.",
-         "Знайте, что успех благоволит только настойчивым,"
-         " поэтому посвятите этот день воспитанию духа.",
-         "Даже если вы не сможете уменьшить влияние"
-         " ретроградного Меркурия, то хотя бы "
-         "доведите дела до конца.",
-         "Не нужно бояться одиноких встреч — сегодня "
-         "то самое время, когда они значат многое.",
-         "Если встретите незнакомца на пути — проявите участие,"
-         " и тогда эта встреча посулит вам приятные хлопоты."]
+first = ["Today is the perfect day for new beginnings.",
+         "The optimal day to decide on a bold act!",
+         "Be careful, the stars today"
+         " may affect your financial condition.",
+         "The best time to start"
+         " new relationships or deal with old ones.",
+         "A fruitful day in order to "
+         "deal with the backlog."]
+second = ["But remember that even in this case, you must not forget about",
+          "If you go out of town, think about",
+          "Those who today aim to fulfill "
+          "a lot of things to remember about",
+          "If you are fatigued, pay attention to",
+          "Remember that thoughts are material, which means you"
+          " during the day you need to constantly think about"]
+second_add = ["relationships with friends and family.",
+              "work and business matters, "
+              "who can so inappropriately interfere with the plans.",
+              "yourself and your health, otherwise to "
+              "in the evening, complete discord is possible.",
+              "everyday issues - especially those,"
+              " which you did not complete yesterday.",
+              "rest so as not to turn yourself into"
+              " a driven horse at the end of the month."]
+third = ["Evil tongues may tell you otherwise"
+         " but today you don't need to listen to them.",
+         "Know that success favors only the persistent,"
+         " therefore, devote this day to the education of the spirit.",
+         "Even if you can't lessen the impact"
+         " retrograde Mercury, then at least "
+         "get things done.",
+         "No need to be afraid of lonely meetings - today "
+         "the very time when they mean a lot.",
+         "If you meet a stranger on the way, be involved.,"
+         " and then this meeting will promise you pleasant chores."]
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
 
-    if message.text == "Привет":
+    if message.text == "Hi":
 
         bot.send_message(message.from_user.id,
-        "Привет, сейчас я расскажу тебе гороскоп на сегодня.")
+        "Hi, now I will tell you the horoscope for today.")
 
         keyboard = types.InlineKeyboardMarkup()
 
-        key_oven = types.InlineKeyboardButton(text='Овен', callback_data='zodiac')
+        key_oven = types.InlineKeyboardButton(text='Aries', callback_data='zodiac')
 
         keyboard.add(key_oven)
-        key_telec = types.InlineKeyboardButton(text='Телец', callback_data='zodiac')
+        key_telec = types.InlineKeyboardButton(text='Taurus', callback_data='zodiac')
         keyboard.add(key_telec)
-        key_bliznecy = types.InlineKeyboardButton(text='Близнецы', callback_data='zodiac')
+        key_bliznecy = types.InlineKeyboardButton(text='Gemini', callback_data='zodiac')
         keyboard.add(key_bliznecy)
-        key_rak = types.InlineKeyboardButton(text='Рак', callback_data='zodiac')
+        key_rak = types.InlineKeyboardButton(text='Cancer', callback_data='zodiac')
         keyboard.add(key_rak)
-        key_lev = types.InlineKeyboardButton(text='Лев', callback_data='zodiac')
+        key_lev = types.InlineKeyboardButton(text='Leo', callback_data='zodiac')
         keyboard.add(key_lev)
-        key_deva = types.InlineKeyboardButton(text='Дева', callback_data='zodiac')
+        key_deva = types.InlineKeyboardButton(text='Virgo', callback_data='zodiac')
         keyboard.add(key_deva)
-        key_vesy = types.InlineKeyboardButton(text='Весы', callback_data='zodiac')
+        key_vesy = types.InlineKeyboardButton(text='Libra', callback_data='zodiac')
         keyboard.add(key_vesy)
-        key_scorpion = types.InlineKeyboardButton(text='Скорпион', callback_data='zodiac')
+        key_scorpion = types.InlineKeyboardButton(text='Scorpio', callback_data='zodiac')
         keyboard.add(key_scorpion)
-        key_strelec = types.InlineKeyboardButton(text='Стрелец', callback_data='zodiac')
+        key_strelec = types.InlineKeyboardButton(text='Sagittarius', callback_data='zodiac')
         keyboard.add(key_strelec)
-        key_kozerog = types.InlineKeyboardButton(text='Козерог', callback_data='zodiac')
+        key_kozerog = types.InlineKeyboardButton(text='Capricorn', callback_data='zodiac')
         keyboard.add(key_kozerog)
-        key_vodoley = types.InlineKeyboardButton(text='Водолей', callback_data='zodiac')
+        key_vodoley = types.InlineKeyboardButton(text='Aquarius', callback_data='zodiac')
         keyboard.add(key_vodoley)
-        key_ryby = types.InlineKeyboardButton(text='Рыбы', callback_data='zodiac')
+        key_ryby = types.InlineKeyboardButton(text='Pisces', callback_data='zodiac')
         keyboard.add(key_ryby)
         # Показываем все кнопки сразу и пишем сообщение о выборе
-        bot.send_message(message.from_user.id, text='Выбери свой знак зодиака', reply_markup=keyboard)
+        bot.send_message(message.from_user.id, text='Choose your zodiac sign', reply_markup=keyboard)
     elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши Привет")
+        bot.send_message(message.from_user.id, "Type Hi")
     else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+        bot.send_message(message.from_user.id, "I don’t understand you. Type / help.")
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
 
